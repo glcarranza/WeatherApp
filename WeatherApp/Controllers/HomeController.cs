b363f3a0369dadc47fc1b5441a9c200e5b9d7738
MiPc <MiPc@MIPC-PC>
2021-01-26 10:05:22 -0300

    Add project files.

---
+++ WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using WeatherApp.Models;
+using System.Web.Configuration;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetPaises()
+        {
+            string urlServicio = WebConfigurationManager.AppSettings["urlServicio"];
+            var result = DAL.DataAccess.setConnection(urlServicio, "api/Pais");
+            var listaPaises = JsonConvert.DeserializeObject<List<Paises>>(result);
+            return Json(listaPaises, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetCiudades(int idPais)
+        {
+            string urlServicio = WebConfigurationManager.AppSettings["urlServicio"];
+            var result = DAL.DataAccess.setConnection(urlServicio, "api/Ciudad?IdPais=" + idPais);
+            var listaCiudades = JsonConvert.DeserializeObject<List<Ciudades>>(result);
+            return Json(listaCiudades, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetClimaPorPaisCiudad(int idPais, int idCiudad)
+        {
+            string urlServicio = WebConfigurationManager.AppSettings["urlServicio"];
+            var result = DAL.DataAccess.setConnection(urlServicio, "api/Clima?IdPais=" + idPais + "&IdCiudad=" + idCiudad);
+            var listaCiudades = JsonConvert.DeserializeObject<List<ClimaPorPaisCiudad>>(result);
+            return Json(listaCiudades, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
